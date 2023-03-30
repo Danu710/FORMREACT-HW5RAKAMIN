921fd8f879b35eab98d437d4ec369873c925e2fd
Danu710 <danukr710@gmail.com>
2023-03-31 01:06:44 +0700

    fix bug

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,7 @@
 import React from "react";
-import Login from "../component/Login";
-import Register from "../component/Register";
 import Home from "../component/Home";
 import Footer from "../component/Footer";
 import Navbar from "../component/Navbar";
-import TableComponent from "../component/TableComponent";
 
 const Homepage = () => {
   return (
